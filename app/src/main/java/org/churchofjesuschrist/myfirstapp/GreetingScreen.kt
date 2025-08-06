e65796fe573eb5c46f3fedce6c5e6b028302c320
Steve Tripp <steven.m.tripp@gmail.com>
2025-08-06 16:17:42 -0600

    Initialize Android project structure and UI components

---
+++ app/src/main/java/org/churchofjesuschrist/myfirstapp/GreetingScreen.kt
@@ -0,0 +1,23 @@
+package org.churchofjesuschrist.myfirstapp
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.churchofjesuschrist.myfirstapp.ui.theme.MyFirstAppTheme
+
+@Composable
+fun GreetingScreen(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingScreenPreview() {
+    MyFirstAppTheme {
+        GreetingScreen("Android")
+    }
+}
---
+++ app/src/main/java/org/churchofjesuschrist/myfirstapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package org.churchofjesuschrist.myfirstapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.churchofjesuschrist.myfirstapp.ui.theme.MyFirstAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyFirstAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GreetingScreen(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
