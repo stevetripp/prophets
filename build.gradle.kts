f2452f1378d6ffc5e7acd577a97e6fabb8434a69
Steve Tripp <steven.m.tripp@gmail.com>
2025-10-14 15:59:58 -0600

    Add Ben Manes Gradle Versions plugin and configure dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application.plugin) apply false
     alias(libs.plugins.androidx.room.plugin) apply false
+    alias(libs.plugins.ben.manes.versions.plugin)
     alias(libs.plugins.hilt.android.plugin) apply false
     alias(libs.plugins.kotlin.android.plugin) apply false
     alias(libs.plugins.kotlin.compose.plugin) apply false
@@ -9,3 +12,26 @@ plugins {
     alias(libs.plugins.ksp.plugin) apply false
 }
 // Ensure pluginManagement repositories are set in settings.gradle.kts
+
+// ===== Gradle Dependency Check =====
+// ./gradlew dependencyUpdates -Drevision=release
+// ./gradlew dependencyUpdates -Drevision=release --refresh-dependencies
+//
+// ./gradlew app:dependencyInsight --configuration debugRuntimeClasspath --dependency androidx.room
+// ./gradlew shared:dependencyInsight --configuration commonMainApiDependenciesMetadata --dependency androidx.room
+// ./gradlew shared:resolvableConfigurations | grep "^Configuration"
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf { isNonStable(version = candidate.version, includeStablePreRelease = true) }
+}
+
+fun isNonStable(version: String, includeStablePreRelease: Boolean): Boolean {
+    val stablePreReleaseKeyword = false //  listOf("RC", "BETA").any { version.uppercase().contains(it) }
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+$".toRegex()
+    val isStable = if (includeStablePreRelease) {
+        stableKeyword || regex.matches(version) || stablePreReleaseKeyword
+    } else {
+        stableKeyword || regex.matches(version)
+    }
+    return isStable.not()
+}
